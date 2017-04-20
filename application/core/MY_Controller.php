f0b6f125d4fbda108334ab8162385568c8152451
puntthis <jlawhorn@gmx.com>
2017-04-20 14:00:48 -0400

    Adding changes to get PHP_AUTH variables working in production

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -17,6 +17,8 @@ class Api extends CI_Controller {
 	*/
 	private function _verify_user()
 	{
+		list($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']) = explode(':', base64_decode(substr($_SERVER['HTTP_AUTHORIZATION'], 6)));
+		
 		if (isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW']))
 		{
 			$user_verified = $this->Api_user_m->user_verified($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']);
