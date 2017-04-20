a9f60e318270341e41c4d25dd2e634ecc667babb
puntthis <jlawhorn@gmx.com>
2017-04-20 13:25:40 -0400

    Updating Test controller to use MY_Controller and spacing

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -1,12 +1,12 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Test extends CI_Controller {
+class Test extends MY_Controller {
 
 	public function __construct()
 	{
-    	parent::__construct();
-    }
+		parent::__construct();
+	}
 	
 	public function index()
 	{
